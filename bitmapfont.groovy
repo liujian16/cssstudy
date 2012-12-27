e9a198bbe74e03a8d862e8780c47d9855094e678
liujian16 <liujian16@gmail.com>
2012-12-27 22:54:07 +0800

    output chinese character as int array. show it using javascript.

--- bitmapfont.groovy
+++ bitmapfont.groovy
@@ -3,7 +3,7 @@ import java.awt.*;
 import javax.imageio.*;
 import javax.imageio.stream.*;
 image = new BufferedImage(16,16,BufferedImage.TYPE_INT_RGB)
-s = '¡ı'
+s = 'Œ∫'
 g = image.getGraphics()
 font = new Font("ÀŒÃÂ",Font.PLAIN,16)
 g.setFont(font)
@@ -11,55 +11,23 @@ g.drawString(s,0,15)
 f = new FileImageOutputStream(new File('c:\\¡ı.png'))
 ImageIO.write(image,"png",f)
 f.close()
-print """<!DOCTPYE html>
-</html>
-<head>
-<style>
-
-
-
-div.black_cell{
-//float:left;
-width:20px;
-height:20px;
-background-color:blue;
-display:inline-block;
-margin:0px;
-}
-div.white_cell{
-//float:left;
-width:20px;
-height:20px;
-background-color:black;
-display:inline-block;
-margin:0px;
-}
-div.row{
-//clear:both;
-padding:0px;
-}
-</style>
-</head>
-<body>
-"""
+i =0
 16.times{x->
-    print """<div class="row">"""
-    i = 0 ;
+       
+    if(x%2==0)
+        i = 0 ;
     16.times{y->
        // println(x+" "+y)
         rgb = image.getRGB(y,x) 
         color = new Color(rgb)      
         isWhite = color.equals(Color.white)
-        i = i | (1<<y)           
-        print(
-            """<div class="${isWhite?"white_cell":"black_cell"}"></div>"""
-        )
-    }
-//    println i
-    print("</div>")
+        if(isWhite)
+            i = i | (1<<(31-(x%2)*16-y))           
 
+    }
+    if(x%2==1){
+        printf("%d, ",i)
+    //    printf("%x, ",i)
+        }
 }
-print """</body>
-</html>
-"""
-    
+ 
